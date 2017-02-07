fe03e28b92c09a8cc39314a0e151a131736497a3
Julia Ouimet <julia.ouimet@gmail.com>
2017-02-07 17:03:19 -0500

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var quizLength = '3';
-var qSelector = 0;
-var questionNumber = 1;
+var qSelector = '0';
+var questionNumber = '1';
 var score = 
 	{correct: '0',};
 
@@ -36,20 +36,24 @@ var currentAnswer = (questionsAndAnswers[qSelector].answer);
 var currentScore = (score.correct)
 
 
-function initiateGame() {
-	$(".begin").on("click", function() {
-    	$(".question-template").removeClass("hidden");
-    	$(".welcome-message").addClass("hidden");
-    	$(".question").html(currentQuestion);
+function showQuestion() {
+	    $(".question").html(currentQuestion);
 		$("#o1").html(currentOption1);
 		$("#o2").html(currentOption2);
 		$("#o3").html(currentOption3);
 		$(".upper-notification").html("Your score is " + currentScore + " of " + quizLength);
+}
+
+function initiateGame() {
+	$(".begin").on("click", function() {
+    	$(".question-template").removeClass("hidden");
+    	$(".welcome-message").addClass("hidden");
+    	showQuestion();
 	});
 }
 
 function questionSubmit() {
-	(".submit").on("click", function() {
+	$(".submit").on("click", function() {
 		if ($('input[name="options"]:checked').val() == currentAnswer) {
 			score.correct++;
 			$(".section-paragraph").html("Yeah! You answered the last question correctly!");
@@ -57,9 +61,26 @@ function questionSubmit() {
 	    else {
 	     	$(".section-paragraph").html("Bummer! Your last answer was incorrect.");
 	    };
+	    var qSelector = qSelector++;
+	    var questionNumber = questionNumber++;
+	    showQuestion();
+	    console.log(questionNumber);
+	    console.log(qSelector);
 	});
 }
 
+// end game
+if (questionNumber > quizLength) {
+  $(".question-template").addClass("hidden");
+  $(".section-paragraph").html("Game Over. You scored " + score.correct + " out of " + quizLength);
+  $(".lower-notification").append('<br><button class="reload">Begin Again</button></br>');
+};
+  
+$(".js-results-message").on("click", ".reload", function(){
+  location.reload();
+})
+
 $(function(){
 	initiateGame();
+	questionSubmit();
 })
